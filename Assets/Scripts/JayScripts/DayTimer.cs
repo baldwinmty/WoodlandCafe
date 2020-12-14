895fd6e7ea15726b1c461fc2bde0fcd22334cc06
Jay <s199064@students.aie.edu>
2020-12-14 13:08:22 -0800

    Created the Game Manager and Timer scripts!

---
+++ Assets/Scripts/JayScripts/DayTimer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class DayTimer : MonoBehaviour
+{
+    // TODO: Should hold a public reference to a Light(s) component
+    // so it can be changed over the course of the day.
+
+    [Tooltip("Time in seconds that the day should go on for.")]
+    public float dayLength = 300f;
+    private float dayRemaining;
+    private bool executedEndEvents = false;
+
+    // TODO: Style the text in the timer UI to appear like an actual clock.
+    // Ex: Start at 8 am, timer ticks until the day ends at 8 pm (12 minutes IGT);
+    [Tooltip("Place the UI object you want to display the time here!")]
+    public TextMeshProUGUI timeUI;
+
+    [Tooltip("Once the timer ends, these events will be triggered once.")]
+    public UnityEvent OnTimerEnd;
+
+    void Start()
+    {
+        dayRemaining = dayLength;
+        executedEndEvents = false;
+    }
+
+
+    void Update()
+    {
+        if (dayRemaining > 0)
+        {
+            dayRemaining -= Time.deltaTime;
+            timeUI.text = dayRemaining.ToString("0:00");
+        }
+        else
+        {
+            if (!executedEndEvents)
+            {
+                Debug.Log("Time's up!");
+                OnTimerEnd.Invoke();
+                executedEndEvents = true;
+            }
+        }
+    }
+
+    public void Ping()
+    {
+        Debug.Log("Pong!");
+    }
+}
---
+++ Assets/Scripts/JayScripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Static class meant for holding anything that needa to persist between scenes or levels.
+    // Also holds functionality for scene switching.
+
+    // This can be used in a variety of ways, like getting music to persist betwee scenes,
+    // keeping track of variables used in one scene to be used in another, etc.
+
+    private static GameManager _GM;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            // If we have no current game manager...
+            if (_GM == null)
+            {
+                // Find the first one we can!
+                _GM = FindObjectOfType<GameManager>();
+
+                // If that still doesn't work...
+                if (_GM == null)
+                {
+                    // Create one!
+                    GameObject gm = new GameObject("GM");
+                    _GM = gm.AddComponent<GameManager>();
+                }
+            }
+
+            return _GM;
+        }
+        private set
+        {
+            _GM = value;
+        }
+    }
+
+    private void Awake()
+    {
+        // If _GM is already populated and it's value isn't equal to this one...
+        if (_GM != null && _GM != this)
+        {
+            // Destroy this one! It's a copy, and we only need 1 GM at a given point!
+            Destroy(gameObject);
+            return;
+        }
+
+        // Set the _GM to be this instance, and make sure it can't be deleted when we switch scenes!
+        _GM = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ChangeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
