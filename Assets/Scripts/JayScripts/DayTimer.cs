94da01e5cb2f835715df2941541e7d885ce46525
Jay <s199064@students.aie.edu>
2020-12-15 15:29:39 -0800

    end of day UI + timer + score keeper
    everything is linked together now. timer on the EoD UI is in seconds! (default 300 seconds is 5 minutes)

--- Assets/Scripts/JayScripts/DayTimer.cs
+++ Assets/Scripts/JayScripts/DayTimer.cs
@@ -14,12 +14,12 @@ public class DayTimer : MonoBehaviour
     private float remainingTime = 0f;
     private bool executedEndEvents = false;
 
-    [Tooltip("Toggle whether or not to display the time UI to the player.")]
-    public bool useUI = false;
-    [Tooltip("Place the UI object you want to display the time here!")]
-    public TextMeshProUGUI timeUI;
-    [Range(1, 12)] [Tooltip("What time to start the human-readable clock at.")]
-    public int startClockTime = 8;
+    //[Tooltip("Toggle whether or not to display the time UI to the player.")]
+    //public bool useUI = false;
+    //[Tooltip("Place the UI object you want to display the time here!")]
+    //public TextMeshProUGUI timeUI;
+    //[Range(1, 12)] [Tooltip("What time to start the human-readable clock at.")]
+    //public int startClockTime = 8;
     //public bool amTime = true;
 
     // Unity Events let you add multiple calls from various other objects onto a function
@@ -32,7 +32,7 @@ public class DayTimer : MonoBehaviour
         remainingTime = 0f;
         executedEndEvents = false;
 
-        timeUI.gameObject.SetActive(useUI);
+        //timeUI.gameObject.SetActive(useUI);
     }
 
     void Update()
@@ -46,10 +46,10 @@ public class DayTimer : MonoBehaviour
             // Style the text in the timer UI to appear like an actual clock.
             // Ex: Start at 8 am, timer ticks until the day ends at 8 pm (12 minutes IGT)
             // The visual clock is of course just an illusion of time for the player to understand.
-            if (useUI)
-            {
-                TimeUIFormat(remainingTime, startClockTime);
-            }
+            //if (useUI)
+            //{
+            //    TimeUIFormat(remainingTime, startClockTime);
+            //}
         }
         else
         {
@@ -65,17 +65,17 @@ public class DayTimer : MonoBehaviour
     }
 
     // TODO: Fix this. For some reason getting past 13 is impossible.
-    private void TimeUIFormat(float remainingTime, float addedTime)
-    {
-        float hour = ((remainingTime / 120f) + addedTime) % 12f;
-        float minute = remainingTime % 60;
+    //private void TimeUIFormat(float remainingTime, float addedTime)
+    //{
+    //    float hour = ((remainingTime / 120f) + addedTime) % 12f;
+    //    float minute = remainingTime % 60;
 
-        timeUI.text = remainingTime.ToString("0.00") + "\n";
-        timeUI.text += string.Format("{0}:{1}"/*  {2}"*/, 
-            (hour).ToString("00").Replace("00", "12"),
-            (minute).ToString("00").Replace("60", "59")/*,*/
-            /*am ? "am" : "pm"*/);
-    }
+    //    timeUI.text = remainingTime.ToString("0.00") + "\n";
+    //    timeUI.text += string.Format("{0}:{1}"/*  {2}"*/, 
+    //        (hour).ToString("00").Replace("00", "12"),
+    //        (minute).ToString("00").Replace("60", "59")/*,*/
+    //        /*am ? "am" : "pm"*/);
+    //}
 
     // TODO: Test function, remove later.
     public void Ping()
