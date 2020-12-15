11e2af2e7960e7d6e7cdffde95231615578aba61
baldwinmty <baldwinmty@gmail.com>
2020-12-15 15:19:50 -0800

    TylerB
    AudioManager push 1

--- Assets/Scripts/TylerScripts/AudioManager.cs
+++ Assets/Scripts/TylerScripts/AudioManager.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+[System.Serializable]
+
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    public float volume = 1f;
+    public float pitch = 1f;
+
+    private AudioSource source;
+
+    public void SetSource (AudioSource _source)
+    {
+        source = _source;
+            source.clip = clip;
+    }
+
+    public void Play ()
+    {
+        source.volume = volume;
+        source.pitch = pitch;
+        source.Play();
+    }
+}
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    Sound[] sounds;
 }
