cdd985617787c210a0a2f5c777e8f46c850c1ae3
BenGermano <bsg2bsg@yahoo.com>
2020-12-14 17:08:38 -0800

    menu sliding

--- Assets/Scripts/JayScripts/UIButtonFunctions.cs
+++ Assets/Scripts/JayScripts/UIButtonFunctions.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class UIButtonFunctions : MonoBehaviour
 {
+    public GameObject Menu;
+
     public void ChangeScene(int sceneIndex)
     {
         GameManager.Instance.ChangeScene(sceneIndex);
@@ -12,5 +14,29 @@ public class UIButtonFunctions : MonoBehaviour
     public void QuitGame()
     {
         GameManager.Instance.QuitGame();
+    }
+
+    public void MenuSlider()
+    {
+        if(Menu != null)
+        {
+            Animator animator = Menu.GetComponent<Animator>();
+            if(animator != null)
+            {
+                animator.SetBool("Move", true);
+            }
+        }
+    }
+
+    public void MenuSliderUp()
+    {
+        if (Menu != null)
+        {
+            Animator animator = Menu.GetComponent<Animator>();
+            if (animator != null)
+            {
+                animator.SetBool("Move", false);
+            }
+        }
     }
 }
