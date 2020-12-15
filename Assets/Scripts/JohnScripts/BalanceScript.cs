39cb33a48ba1632f93218e265e7950229666993b
JohnathanBlaikie <44881101+JohnathanBlaikie@users.noreply.github.com>
2020-12-15 14:43:30 -0800

    2D cup catch prefab

---
+++ Assets/Scripts/JohnScripts/BalanceScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceScript : MonoBehaviour
+{
+    public GameObject balanceGO;
+    public Rigidbody balanceRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        balanceRB.rotation = Quaternion.Euler(Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/JohnScripts/CupCatchScript.cs
+++ Assets/Scripts/JohnScripts/CupCatchScript.cs
@@ -7,10 +7,12 @@ public class CupCatchScript : MonoBehaviour
 {
     public int pointCounter;
     public Text pointDisplay;
+    public Button startButton;
 
     public GameObject snuggie;
     public GameObject cup;
 
+    bool hasStarted = false;
     private bool hasWon = false;
 
     //public MiniGameManager miniGameManager;
@@ -29,41 +31,47 @@ public class CupCatchScript : MonoBehaviour
     {
         //Saving for later:
         //pointDisplay.text = miniGameManager.score;
+        cup.SetActive(false);
         snuggie.transform.position = Input.mousePosition;
         snuggieRB = snuggie.GetComponent<Rigidbody2D>();
+        cup.GetComponent<Rigidbody2D>().isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (touchingSides < 2)
+       
+        if (hasStarted)
         {
-            Vector3 mousePos = Input.mousePosition;
-            Ray castPoint = orthoCam.ScreenPointToRay(mousePos);
-            RaycastHit hit;
-            if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+            if (touchingSides < 2)
             {
-                snuggie.transform.position = hit.point;
-                //snuggieRB.velocity = ((transform.right * mousePos.x) + (transform.forward * mousePos.y)) / Time.deltaTime;
-                snuggieRB.velocity = (hit.point - snuggie.transform.position) * 10;
-            }
+                Vector3 mousePos = Input.mousePosition;
+                Ray castPoint = orthoCam.ScreenPointToRay(mousePos);
+                RaycastHit hit;
+                if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+                {
+                    snuggie.transform.position = hit.point;
+                    //snuggieRB.velocity = ((transform.right * mousePos.x) + (transform.forward * mousePos.y)) / Time.deltaTime;
+                    snuggieRB.velocity = (hit.point - snuggie.transform.position) * 10;
+                }
 
-            if (cup.transform.position.y < -10)
-            {
-                FailedToCatch();
+                if (cup.transform.position.y < -10)
+                {
+                    FailedToCatch();
+                }
             }
-        }
-        else
-        {
-            if (hasWon == false)
+            else
             {
-                Debug.Log("WIN!");
-                pointCounter += winReward;
-                hasWon = true;
+                if (hasWon == false)
+                {
+                    Debug.Log("WIN!");
+                    pointCounter += winReward;
+                    hasWon = true;
+                }
             }
-        }
-        pointDisplay.text = pointCounter.ToString();
+            pointDisplay.text = pointCounter.ToString();
 
+        }
     }
     void FailedToCatch()
     {
@@ -72,5 +80,12 @@ public class CupCatchScript : MonoBehaviour
         cup.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
         cup.transform.rotation = Quaternion.Euler(Vector3.zero);
         cup.transform.position = new Vector3(Random.Range(minXSpawn, maxXSpawn), ySpawn);
+    }
+    public void ButtonPressed()
+    {
+        hasStarted = true;
+        cup.SetActive(true);
+        cup.GetComponent<Rigidbody2D>().isKinematic = false;
+        startButton.enabled = false;
     }
 }
