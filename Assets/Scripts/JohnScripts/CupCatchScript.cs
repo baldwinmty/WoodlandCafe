880dd0ebab7b1816279a8bb6d75b5b3066411623
JohnathanBlaikie <44881101+JohnathanBlaikie@users.noreply.github.com>
2020-12-15 11:01:43 -0800

    Worked on cup game, little janky still

--- Assets/Scripts/JohnScripts/CupCatchScript.cs
+++ Assets/Scripts/JohnScripts/CupCatchScript.cs
@@ -11,28 +11,66 @@ public class CupCatchScript : MonoBehaviour
     public GameObject snuggie;
     public GameObject cup;
 
+    private bool hasWon = false;
+
+    //public MiniGameManager miniGameManager;
+
     public Camera orthoCam;
 
-    private Vector3 correctedMousePos;
+    public float minXSpawn, maxXSpawn, ySpawn;
 
+    public int winReward, missPenalty;
+
+    [HideInInspector]
+    public int touchingSides;
+    public Rigidbody2D snuggieRB;
     // Start is called before the first frame update
     void Start()
     {
+        //Saving for later:
+        //pointDisplay.text = miniGameManager.score;
         snuggie.transform.position = Input.mousePosition;
+        snuggieRB = snuggie.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
-        Ray castPoint = orthoCam.ScreenPointToRay(mousePos);
-        RaycastHit hit;
-        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        if (touchingSides < 2)
         {
-            snuggie.transform.position = hit.point;
+            Vector3 mousePos = Input.mousePosition;
+            Ray castPoint = orthoCam.ScreenPointToRay(mousePos);
+            RaycastHit hit;
+            if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+            {
+                snuggie.transform.position = hit.point;
+                //snuggieRB.velocity = ((transform.right * mousePos.x) + (transform.forward * mousePos.y)) / Time.deltaTime;
+                snuggieRB.velocity = (hit.point - snuggie.transform.position) * 10;
+            }
+
+            if (cup.transform.position.y < -10)
+            {
+                FailedToCatch();
+            }
         }
+        else
+        {
+            if (hasWon == false)
+            {
+                Debug.Log("WIN!");
+                pointCounter += winReward;
+                hasWon = true;
+            }
+        }
+        pointDisplay.text = pointCounter.ToString();
 
-        //snuggie.transform.position = correctedMousePos;
-
+    }
+    void FailedToCatch()
+    {
+        pointCounter -= missPenalty;
+        
+        cup.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        cup.transform.rotation = Quaternion.Euler(Vector3.zero);
+        cup.transform.position = new Vector3(Random.Range(minXSpawn, maxXSpawn), ySpawn);
     }
 }
---
+++ Assets/Scripts/JohnScripts/CupHolsterScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupHolsterScript : MonoBehaviour
+{
+    public CupCatchScript cCS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    //private void Update()
+    //{
+    //    GetComponent<Rigidbody2D>().velocity = cCS.snuggieRB.velocity;
+    //}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("CupTrigger"))
+        {
+            cCS.touchingSides++;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("CupTrigger"))
+        {
+            cCS.touchingSides--;
+        }
+    }
+}
