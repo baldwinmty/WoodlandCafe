d315db1be1e9c85914dd9ad0773da91c7173e4e8
baldwinmty <baldwinmty@gmail.com>
2020-12-15 15:50:03 -0800

    TylerB
    GLASS

--- Assets/FrogAnimator.cs
+++ Assets/FrogAnimator.cs
@@ -1,30 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class FrogAnimator : MonoBehaviour
-{
-    public Animator animator;
-    private bool IsSitting = false;
-    private bool IsHappy = false;
-    private bool IsSad = false;
+//public class FrogAnimator : MonoBehaviour
+//{
+//    public Animator animator;
+//    private bool IsSitting = false;
+//    private bool IsHappy = false;
+//    private bool IsSad = false;
 
-    void start()
-    {
-        animator = GetComponent<Animator>();
-    }
+//    void start()
+//    {
+//        animator = GetComponent<Animator>();
+//    }
 
-    void Update ()
-    {
-        if(Input.GetKeyDown(KeyCode.Space) && !IsSitting)
-        {
-            animator.SetBool("IsSitting", true);
-            IsSitting = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.Space) && IsSitting)
-        {
-            animator.SetBool("IsSitting", false);
-            IsSitting = false;
-        }
-    }
-}
+//    void Update ()
+//    {
+//        if(Input.GetKeyDown(KeyCode.Space) && !IsSitting)
+//        {
+//            animator.SetBool("IsSitting", true);
+//            IsSitting = true;
+//        }
+//        else if (Input.GetKeyDown(KeyCode.Space) && IsSitting)
+//        {
+//            animator.SetBool("IsSitting", false);
+//            IsSitting = false;
+//        }
+//    }
+//}
--- Assets/Scripts/TylerScripts/AudioManager.cs
+++ Assets/Scripts/TylerScripts/AudioManager.cs
@@ -6,8 +6,15 @@ public class Sound
     public string name;
     public AudioClip clip;
 
+    [Range(0f,1f)]
     public float volume = 1f;
+    [Range(0.5f, 1.5f)]
     public float pitch = 1f;
+
+    [Range(0f, .5f)]
+    public float randomVolume = .1f;
+    [Range(0f, .5f)]
+    public float randomPitch = .1f;
 
     private AudioSource source;
 
@@ -20,13 +27,55 @@ public class Sound
     public void Play ()
     {
         source.volume = volume;
-        source.pitch = pitch;
+        source.pitch = pitch * (1 + Random.Range(-randomPitch / 2f, randomPitch / 2f));
         source.Play();
     }
 }
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance;
+
     [SerializeField]
     Sound[] sounds;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("More than 1 audio manager in scene");
+        }
+        else
+        {
+        instance = this;
+        }
+    }
+
+    void Start ()
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].name);
+            sounds[i].SetSource (_go.AddComponent<AudioSource>());
+        }
+
+        PlaySound("Loop");
+    }
+
+    public void PlaySound (string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == _name)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+
+        // no sound w/ _name
+        Debug.Log("AudioManager: no sound of that name. " + _name);
+
+    }
+
 }
