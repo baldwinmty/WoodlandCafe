ba75b06bdc28fc42aff8ab439a5488b11c7edfe0
baldwinmty <baldwinmty@gmail.com>
2020-12-15 13:00:00 -0800

    TylerB
    Animator: Humble Beginnings

---
+++ Assets/FrogAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogAnimator : MonoBehaviour
+{
+    public Animator animator;
+    private bool IsSitting = false;
+    private bool IsHappy = false;
+    private bool IsSad = false;
+
+    void start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && !IsSitting)
+        {
+            animator.SetBool("IsSitting", true);
+            IsSitting = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && IsSitting)
+        {
+            animator.SetBool("IsSitting", false);
+            IsSitting = false;
+        }
+    }
+}
