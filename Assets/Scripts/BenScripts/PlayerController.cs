952643f098a144bd41b3a46aeabf6206b9ed6da4
BenGermano <bsg2bsg@yahoo.com>
2020-12-15 17:10:09 -0800

    Player controller on prefab

---
+++ Assets/Scripts/BenScripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed;
+    public float smoothTime;
+    float smoothVel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(dir.magnitude >= 0.1f)
+        {
+            float tarAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, tarAngle, ref smoothVel, smoothTime);
+            transform.rotation = Quaternion.Euler(0f, tarAngle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, tarAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
