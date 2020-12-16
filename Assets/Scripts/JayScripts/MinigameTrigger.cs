e17abd9786fc2ddb2c14c21f0fe1f8f68cd9f2b3
Jay <s199064@students.aie.edu>
2020-12-15 23:15:20 -0800

    spawner stuff:tm:

---
+++ Assets/Scripts/JayScripts/MinigameTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameTrigger : MonoBehaviour
+{
+    private MinigameManager manager;
+    private bool playerInRange = false;
+
+    private void Awake()
+    {
+        manager = FindObjectOfType<MinigameManager>();
+    }
+
+    private void Update()
+    {
+        if (playerInRange)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                manager.TriggerMinigame();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
