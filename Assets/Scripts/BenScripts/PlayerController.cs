19733af0520fca40fe6d984c6957bad029daca32
BenGermano <bsg2bsg@yahoo.com>
2020-12-15 22:58:12 -0800

    owl anims in

--- Assets/Scripts/BenScripts/PlayerController.cs
+++ Assets/Scripts/BenScripts/PlayerController.cs
@@ -21,11 +21,34 @@ public class PlayerController : MonoBehaviour
         if(dir.magnitude >= 0.1f)
         {
             float tarAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            Animator animator = controller.GetComponent<Animator>();
+            if(animator != null)
+            {
+                animator.SetBool("Moving", true);
+            }
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, tarAngle, ref smoothVel, smoothTime);
             transform.rotation = Quaternion.Euler(0f, tarAngle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, tarAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        if (dir.magnitude <= 0f)
+        {
+            Animator animator = controller.GetComponent<Animator>();
+            if (animator != null)
+            {
+                animator.SetBool("Moving", false);
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Animator animator = controller.GetComponent<Animator>();
+            if (animator != null)
+            {
+                animator.SetTrigger("Wave");
+            }
         }
     }
 }
